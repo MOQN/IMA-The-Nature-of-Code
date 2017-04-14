339c6dd2c4f60b1be09a2da0069190506d735e86
moqn <moonjunghyun@gmail.com>
2017-04-14 14:13:13 +0800

    improved oscillation&particleSystem examples

---
+++ week06-oscillation/w06-23-oscillation-spring-twoBalls/Spring.js
@@ -0,0 +1,29 @@
+"use strict";
+
+class Spring {
+  constructor(ballA, ballB, len) {
+    this.ballA = ballA;
+    this.ballB = ballB;
+    this.len = len;
+    this.k = 0.2;
+  }
+  display() {
+    push();
+    stroke(255);
+    line(this.ballA.pos.x, this.ballA.pos.y, this.ballB.pos.x, this.ballB.pos.y);
+    pop();
+  }
+  update() {
+    var vector = p5.Vector.sub(this.ballA.pos, this.ballB.pos);
+    var distance = vector.mag();
+    var direction = vector.copy().normalize();
+    var stretch = distance - this.len;
+
+    // hooke's law
+    var force = direction.copy();
+    force.mult(-1 * this.k * stretch);
+    this.ballA.applyForce(force);
+    force.mult(-1);
+    this.ballB.applyForce(force);
+  }
+}
---
+++ week06-oscillation/w06-23-oscillation-spring-twoBalls/sketch.js
@@ -0,0 +1,26 @@
+var ballA, ballB;
+var spring;
+
+function setup() {
+  createCanvas(500, 600);
+  background(0);
+
+  ballA = new Ball(200, 200);
+  ballB = new Ball(400, 400);
+  spring = new Spring(ballA, ballB, 200);
+}
+
+function draw() {
+  background(0);
+
+  spring.update();
+  spring.display();
+
+  ballA.update();
+  ballA.drag();
+  ballA.display();
+
+  ballB.update();
+  ballB.drag();
+  ballB.display();
+}
