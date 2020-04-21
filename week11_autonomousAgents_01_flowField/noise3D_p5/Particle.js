ce1d18e6f444716deb8576e715542388b09b3cd2
moqn <moonjunghyun@gmail.com>
2020-04-21 22:15:17 +0900

    added noise3D with p5.js

--- week11_autonomousAgents_01_flowField/noise3D_p5/Particle.js
+++ week11_autonomousAgents_01_flowField/noise3D_p5/Particle.js
@@ -27,26 +27,25 @@ class Particle {
   applyNoise3D() {
     // get a noise value(range from -1 to 1) based on this particle's 3D position
     let noiseFreq = 0.01;
-    let noiseValue = perlin.noise(
-      (this.pos.x + frameCount) * noiseFreq,
-      (this.pos.y + frameCount) * noiseFreq,
-      (this.pos.z + frameCount) * noiseFreq
-    );
-    
+    let freqX = (this.pos.x + frameCount) * noiseFreq;
+    let freqY = (this.pos.y + frameCount) * noiseFreq;
+    let freqZ = (this.pos.z + frameCount) * noiseFreq;
+    let noiseValue = map( noise(freqX, freqY, freqZ), 0, 1, -1, 1);
+
     // In order to just create a swinging vector in 3D.
     // we need to map the value from 0 to 1 since the noise value can have a negative vlaue.
-    let x = map(cos(frameCount * 0.02), -1, 1, 0, 1); 
+    let x = map(cos(frameCount * 0.02), -1, 1, 0, 1);
     let y = map(sin(frameCount * 0.02), -1, 1, 0, 1); // a circular movement?
     let z = map(sin(frameCount * 0.027), -1, 1, 0, 1); // any arbitrary value
     let vector = createVector(x,y,z);
-    
+
     // let's only get the direction
-    vector.normalize(); 
-    
+    vector.normalize();
+
     // apply the noise value to the direction
-    // feel free to adjust the magnitude (0.01)
-    vector.mult(noiseValue*0.01);
-    
+    // feel free to adjust the magnitude (0.1)
+    vector.mult(noiseValue*0.1);
+
     // apply it as a force
     this.applyForce( vector );
   }
